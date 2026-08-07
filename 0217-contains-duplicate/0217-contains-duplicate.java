748da45560d78c8ec27e2abe621d2940dade24a7
Garv Raj <154749848+garvraj09@users.noreply.github.com>
2026-08-07 14:01:18 +0530

    Time: 18 ms (64.77%), Space: 108.2 MB (27.08%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        // Brute force
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(nums[i]==nums[j])
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+}

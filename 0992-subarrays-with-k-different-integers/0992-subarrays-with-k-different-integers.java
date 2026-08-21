6cf82194c55b5d8ec953dae44a25b67d37272e3a
Garv Raj <154749848+garvraj09@users.noreply.github.com>
2026-08-21 22:47:09 +0530

    Time: 49 ms (57.06%), Space: 50.5 MB (5.92%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,25 +1,25 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return (find(nums, k) - find(nums, k-1));
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 
-    private int find(int arr[], int k){
-        int l=0, r=0, cnt=0;
+    private int atMost(int[] nums, int k){
         HashMap<Integer, Integer> map = new HashMap<>();
-        while(r < arr.length){
-            map.put(arr[r], map.getOrDefault(arr[r], 0)+1);
+        int l = 0;
+        int c = 0;
+        for(int r = 0; r < nums.length; r++){
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
 
             while(map.size() > k){
-                map.put(arr[l], map.get(arr[l]) - 1);
+                map.put(nums[l], map.get(nums[l]) - 1);
 
-                if(map.get(arr[l]) == 0){
-                    map.remove(arr[l]);
+                if(map.get(nums[l]) == 0){
+                    map.remove(nums[l]);
                 }
                 l++;
             }
-            cnt += r-l+1;
-            r++;
+            c += r - l + 1;
         }
-        return cnt;
+        return c;
     }
 }

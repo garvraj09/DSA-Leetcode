ab514bd2e02ed12f202de1eda26a82d5f8d07da1
Garv Raj <154749848+garvraj09@users.noreply.github.com>
2026-08-30 03:06:40 +0530

    Time: 77 ms (81.68%), Space: 151.8 MB (72.52%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+
+        Integer[] indices = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+
+        Arrays.sort(indices, (a, b) -> Integer.compare(nums[a], nums[b]));
+
+        int start = 0;
+
+        while (start < n) {
+            int end = start;
+
+            while (end + 1 < n &&
+                   nums[indices[end + 1]] - nums[indices[end]] <= limit) {
+                end++;
+            }
+
+            List<Integer> positions = new ArrayList<>();
+            List<Integer> values = new ArrayList<>();
+
+            for (int i = start; i <= end; i++) {
+                positions.add(indices[i]);
+                values.add(nums[indices[i]]);
+            }
+
+            Collections.sort(positions);
+
+            for (int i = 0; i < positions.size(); i++) {
+                nums[positions.get(i)] = values.get(i);
+            }
+
+            start = end + 1;
+        }
+
+        return nums;
+    }
+    }
